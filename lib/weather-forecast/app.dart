be4f6f33b55103b6401812700996a7886890933c
thangtmc73 <thangtm.dev@gmail.com>
2021-04-01 08:23:02 +0700

    feat(weather-forecase): add main page

--- lib/weather-forecast/app.dart
+++ lib/weather-forecast/app.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "./main_page.dart";
 
 class App extends StatelessWidget {
   @override
@@ -8,7 +9,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: MainPage(),
     );
   }
 }
